fd2ecdeb81bda782c32df51f97927366a7901416
Cicero Feijo <cicero.feijo@hp.com>
2019-11-22 15:23:21 -0300

    Add support for pt-br intents

--- index.js
+++ index.js
@@ -25,21 +25,42 @@ app.post('/', urlencodedParser, (req, res) => {
     response: res,
   });
   const conv = assistant.conv();
+  const locale = assistant.locale;
 
   console.log('POST /');
   console.log(`========================== ${assistant.intent} ==========================`);
 
   assistant.handleRequest(() => {
+    // Conversations
+    const dialogs = {
+      'pt-br': {
+        welcome: 'Bem vindo ao The Bird!',
+        say_hello: 'Olá meus amigos!',
+        say_good_bye: 'Tchau meus amigos!',
+        default: 'Eu não entendi...',
+      },
+      en: {
+        welcome: 'Welcome to the Bird!',
+        say_hello: 'Hello my friend !',
+        say_good_bye: 'Goodbye my friend !',
+        default: 'I dont understand...',
+      },
+    };
+
+
     // Here you can change the conversaton behavior according the intent received
     switch (assistant.intent) {
+      case 'Default Welcome Intent':
+        conv.ask(dialogs[locale].welcome);
+        break;
       case 'SayHello':
-        conv.close('Hello my friend !!!');
+        conv.close(dialogs[locale].say_hello);
         break;
       case 'SayGoodbye':
-        conv.close('Goodbye my friend !!!');
+        conv.close(dialogs[locale].say_good_bye);
         break;
       default:
-        conv.close('I dont understand...');
+        conv.close(dialogs[locale].default);
         break;
     }
 
